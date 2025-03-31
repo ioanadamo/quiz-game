cc74597d10c96c77d66c327c847b7064113b8ab9
ioanadamo <daniela932910@gmail.com>
2025-03-31 08:17:31 +0100

    feat: initial commit

---
+++ src/components/Result.tsx
@@ -0,0 +1,21 @@
+import {useQuiz} from '../QuizContext.tsx';
+import {decode} from 'html-entities';
+
+function Result() {
+    
+    const {state} = useQuiz();
+
+    return (
+        <>  
+            {
+                state.userAnswer == state.question?.correct_answer ?
+                    <div className="result correct">&#10003; You answered correctly!</div>
+                :
+                <div className="result incorrect">&#x274C; Your answer is wrong! The correct one was {decode(state.question?.correct_answer)}.</div>
+            }
+            
+        </>
+    )
+}
+
+export default Result
